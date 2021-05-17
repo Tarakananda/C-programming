2effa6a4a90246e0c8d3b6979aafb88602b22b27
S.Tarakananda Reddy <tharaknandhamech@gmail.com>
2021-05-17 17:02:19 +0530

    deleted

--- 4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int a=10,b=5,c=0;
-    a= ++a + ++a;
-    printf("%d\n",a);
-    return 0;
-
-}
--- 50.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int i=85;
-    char ch=i;
-    printf ("%d %d %d %d %d %d\n",'I','L','O','V','E','U');
-    return 0;
-}
